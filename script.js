ea5ddca4509f1e5d54adc8ecaabab847ec1b63b8
Satomi Ichii <sichii@gmail.com>
2021-04-05 13:00:55 -0400

    Add: langage arrtibute checker

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const url = 'https://www.fullstackacademy.com/';
 const fs = require('fs');
-const { checklist, messages, SUCCESS, ERROR } = require('./checklist'); 
+const { checklist, messages, SUCCESS, ERROR } = require('./checklist');
 
 const getHtml = async (url) => {
   const webpage = await fetch(url);
   const html = await webpage.text();
   return html;
-}
+};
 
 // const showHtml = async (url) => {
 //   const html = await getHtml(url);
@@ -24,14 +24,14 @@ const getHtml = async (url) => {
 // }
 // writeToFile(url);
 
-async function applyChecklist(url, checklist){
+async function applyChecklist(url, checklist) {
   const html = await getHtml(url);
   const $ = cheerio.load(html);
-  const responses = {}
+  const responses = {};
 
-  for(const item in checklist){
+  for (const item in checklist) {
     const success = await checklist[item]($);
-    responses[item] = messages[item][(success ? SUCCESS : ERROR)];
+    responses[item] = messages[item][success ? SUCCESS : ERROR];
   }
 
   console.log(responses);
