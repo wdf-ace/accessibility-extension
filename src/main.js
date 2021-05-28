65efd4bf26aad9e0f86154dd3a44fb36dc9a8fb1
Anya Dunaif <anya.dunaif@gmail.com>
2021-05-28 11:10:43 -0400

    graph size update

--- src/main.js
+++ src/main.js
@@ -5,69 +5,77 @@ import { checklist } from '../checklist';
 import { VictoryPie, VictoryLabel } from 'victory';
 
 export const Popup = (props) => {
-  const [responses, setResponses] = useState({})
-  const [responsesByPass, setResponsesByPass] = useState({})
+  const [responses, setResponses] = useState({});
+  const [responsesByPass, setResponsesByPass] = useState({});
 
-  useEffect( async () => {
+  useEffect(async () => {
     setResponses(await applyChecklist(checklist));
-  },[])
+  }, []);
 
   useEffect(() => {
-    let tempResponses = {length: 0, SUCCESS: {}, ERROR: {}} 
-    for(let check in responses){
-      responses[check].SUCCESS 
-      ? tempResponses.SUCCESS[check] = responses[check]
-      : tempResponses.ERROR[check] = responses[check]
+    let tempResponses = { length: 0, SUCCESS: {}, ERROR: {} };
+    for (let check in responses) {
+      responses[check].SUCCESS
+        ? (tempResponses.SUCCESS[check] = responses[check])
+        : (tempResponses.ERROR[check] = responses[check]);
       tempResponses.length += 1;
     }
     setResponsesByPass(tempResponses);
-  },[responses])
+  }, [responses]);
 
-  function calcPercentage () {
-    return Math.round(Object.keys(responsesByPass.SUCCESS).length / responsesByPass.length * 100);
+  function calcPercentage() {
+    return Math.round(
+      (Object.keys(responsesByPass.SUCCESS).length / responsesByPass.length) *
+        100
+    );
   }
 
   return (
     <div>
       <h1>Accessibility Results</h1>
-      {
-        responsesByPass.length && 
+      {responsesByPass.length && (
         <div>
-          <VictoryPie
-            data = {[
-              {x: "Pass", y: calcPercentage()},
-              {x: "Fail", y: 100 - calcPercentage()},
-            ]}
-            innerRadius = {120}
-            style = {{
-              data: {
-                fill: ({datum}) => {
-                  let color;
-                  if(datum.y > 80) color = '#338333'
-                  else if (datum.y > 65) color = '#ff8b3d'
-                  else color = 'red'
-                  return datum.x === "Pass" ? color : "transparent"
-                }
-              },
-            }}
-          />
-          <VictoryLabel
-            textAnchor="middle" verticalAnchor="middle"
-            x={200} y={200}
-            text={`${calcPercentage()}%`}
-            style={{ fontSize: 45 }}
-          />
+          <svg viewBox="0 0 250 250">
+            <VictoryPie
+              standalone={false}
+              width={250}
+              height={250}
+              data={[
+                { x: 'Pass', y: calcPercentage() },
+                { x: 'Fail', y: 100 - calcPercentage() },
+              ]}
+              innerRadius={75}
+              labelRadius={75}
+              style={{
+                data: {
+                  fill: ({ datum }) => {
+                    let color;
+                    if (datum.y > 80) color = '#338333';
+                    else if (datum.y > 65) color = '#ff8b3d';
+                    else color = 'red';
+                    return datum.x === 'Pass' ? color : 'transparent';
+                  },
+                },
+              }}
+            />
+            <VictoryLabel
+              textAnchor="middle"
+              // verticalAnchor="middle"
+              x={125}
+              y={125}
+              text={`${calcPercentage()}%`}
+              style={{ fontSize: 30 }}
+            />
+          </svg>
         </div>
-      }
-      
-      {
-        Object.entries(responses).map((check) => {
-          if(check[1].SUCCESS) {
-            return <p className='passed'>PASSED: {check[1].SUCCESS}</p>
-          }
-          return <p className='failed'>ERROR: {check[1].ERROR}</p>
-        })
-      }
+      )}
+
+      {Object.entries(responses).map((check) => {
+        if (check[1].SUCCESS) {
+          return <p className="passed">PASSED: {check[1].SUCCESS}</p>;
+        }
+        return <p className="failed">ERROR: {check[1].ERROR}</p>;
+      })}
     </div>
   );
 };
